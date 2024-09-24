089e7325d7f333fb240c30c2426a78ee7762e583
Misach <misachenko2135@gmail.com>
2024-09-24 20:17:55 +0300

    init

---
+++ CryptoGenerators/SystemGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoGenerators
+{
+    public class SystemGenerator : IGenerator
+    {
+        public BitArray GenBits(double alpha, ulong length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CryptoGeneratorsTests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using CryptoGenerators;
+
+namespace CryptoGeneratorsTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemGenerator gen = new CryptoGenerators.SystemGenerator();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
